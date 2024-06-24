3561d8633781bda3946edab4628e9685f162a40e
Prajwal Dule <prajwaldule58@gmail.com>
2024-06-25 01:45:24 +0530

    second commit

--- React/mentoraideJobs/jobs/src/data/categorydata.jsx
+++ React/mentoraideJobs/jobs/src/data/categorydata.jsx
@@ -9,41 +9,49 @@ import Frame8 from "../assets/image 8.png";
 
 const categoryData = [
     {
+        id:1,
         icon: Frame1,
         label: "Accounting & Finance Jobs",
         jobs_avail: "234 jobs available"  // Example number of jobs available
     },
     {
+        id:2,
         icon: Frame2,
         label: "IT Jobs",
         jobs_avail: "99 jobs available"  // You can leave it empty if no specific number
     },
     {
+        id:3,
         icon: Frame3,
         label: "Sales Jobs",
         jobs_avail: "120 jobs available"  // Example number of jobs available
     },
     {
+        id:4,
         icon: Frame4,
         label: "Customer Services Jobs",
         jobs_avail: "300 jobs available"  // Example number of jobs available
     },
     {
+        id:5,
         icon: Frame5,
         label: "Engineering Jobs",
         jobs_avail: "201 jobs available"  // You can leave it empty if no specific number
     },
     {
+        id:6,
         icon: Frame6,
         label: "HR & Recruitment Jobs",
         jobs_avail: "180 jobs available"  // Example number of jobs available
     },
     {
+        id:7,
         icon: Frame7,
         label: "Healthcare & Nursing Jobs",
         jobs_avail: "10 jobs available"  // You can leave it empty if no specific number
     },
     {
+        id:8,
         icon: Frame8,
         label: "Hospitality & Catering Jobs",
         jobs_avail: "80 jobs available"  // Example number of jobs available
